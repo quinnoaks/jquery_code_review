0cdd5d12039b3460188d56fd17c1e012cff7e304
Quinn Oaks <quinn.oaks@gmail.com>
2015-10-08 21:31:05 -0700

    Add correction to JS error.

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
     var favorite5Input = $("input#favorite5").val();
 
     $(".name1").text(name1Input);
-    $(".favorite1").text(favorite1);
-    $(".favorite2").text(favorite2);
-    $(".favorite3").text(favorite3);
-    $(".favorite4").text(favorite4);
-    $(".favorite5").text(favorite5);
+    $(".favorite1").text(favorite1Input);
+    $(".favorite2").text(favorite2Input);
+    $(".favorite3").text(favorite3Input);
+    $(".favorite4").text(favorite4Input);
+    $(".favorite5").text(favorite5Input);
 
     $("#favlist").show();
 
